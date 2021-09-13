f9b61355be87269be5e0cf9ce1282161fcf179bf
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2021-09-13 19:40:44 +0700

    Membuat views

---
+++ application/views/contoh_view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh View</title>
+</head>
+
+<body>
+    <h1>Contoh View pada Codeigniter 3</h1>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod necessitatibus optio illo voluptatem nam possimus aspernatur voluptatibus modi exercitationem eligendi! Aut, quo vel! Suscipit!</p>
+</body>
+
+</html>
